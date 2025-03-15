f164ac95cb6a395395b749953fa86a7ae2bf607b
Rogerio Almeida <roger.ngt3494@gmail.com>
2025-03-15 10:41:33 -0300

    Removido diretório pcsysten

--- pcsysten/eslint.config.js
+++
@@ -1,6 +0,0 @@
-[build];
-base = "client";
-publish = "client/build";
-command = "CI=false npm run build"[functions];
-directory = "client/netlify/functions"[context.production.environment];
-NODE_ENV = "production";
